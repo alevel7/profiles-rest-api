d941538ab9b669c14846b636769f3ca6c666bed4
Taiwo kazeem <kazeem@kobo360.com>
2023-04-06 12:28:01 +0100

    add sample get view

--- profiles_proj/profiles_api/views.py
+++ profiles_proj/profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """TEST API VIEW"""
+
+    def get(self, request, format=None):
+        """returns a list of features"""
+        an_apiview = [
+            "uses http methods as functions",
+            "is similar to traditional django",
+            "gives most control over application logic",
+            "is mapped manually to urls",
+        ]
+        return Response({"message": "Hello", "data": an_apiview})
