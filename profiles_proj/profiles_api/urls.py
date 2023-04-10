2f5145a2fee8cf4bb3ed3dad53f3a0b0485070d6
Taiwo kazeem <kazeem@kobo360.com>
2023-04-10 15:07:12 +0100

    added user profile feed api

--- profiles_proj/profiles_api/urls.py
+++ profiles_proj/profiles_api/urls.py
@@ -7,8 +7,10 @@ router = DefaultRouter()
 
 router.register("profiles_set", views.HelloViewSet, basename="profile")
 router.register("profile", views.UserProfileViewSet)
+router.register("feed", views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path("profiles_view/", views.HelloApiView.as_view()),
+    path("login/", views.UserLoginApiView.as_view()),
     path("", include(router.urls)),
 ]
--- profiles_proj/profiles_api/views.py
+++ profiles_proj/profiles_api/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializer
 from profiles_api import models
@@ -100,3 +104,22 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     )
     serializer_class = serializer.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """handles creating , reading, updating profile feed items"""
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, permissions.UpdateOwnStatus]
+    serializer_class = serializer.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
